251cfb01683c1ed7e7ae7164d5601c2a965052ba
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2021-11-04 10:37:14 +0100

    Update laFruteria.php

--- Tarea_laFruteria/laFruteria.php
+++ Tarea_laFruteria/laFruteria.php
@@ -1,28 +1,20 @@
 <?php
-/*EJERCICIO DE HACER CARRITO DE LA COMPRA
-PRIMER FORMULARIO MUESTRA EL FORMULARIO DE CLIENTE
-SEGUNDO FORMULARIO MUESTRA LA ELECCIÓN DE LOS PRODUCTOS Y EL NOMBRE DEL CLIENTE QUE QUEDA ALMACENADO EN SESIÓN
-TERCER FORMULARIO SE ACTIVA CON EL BOTÓN ANOTAR Y MUESTRA EL CARRITO DE LA COMPRA QUE SE VA ALMACENANDO EN UN ARRAY DE SESIÓN
-CUARTO FORMULARIO SE ACTIVA CON BOTÓN TERMINAR Y MUESTRA CARRITO DE LA COMPRA Y BOTÓN NUEVO CLIENTE
-QUINTO FORMULARIO ES EL PRIMER FORMULARIO INICIAL QUE SE ACTIVA AL PULSAR NUEVO CLIENTE*/
 //Iniciamos sesión
 session_start();
     $terminar=false;
     
 //Si no existe cliente se cogen los datos por get, se limpian y se crea sesión de cliente
-if (!isset($_SESSION["cliente_"])) {
+
+if (isset ($_GET['cliente']) ) {
     
-    $_SESSION["cliente_"] = "";
     $nombre = $_GET['cliente'];
     $nombre = trim($nombre);
     $nombre = htmlspecialchars($nombre);
     $nombre = stripslashes($nombre);
     $_SESSION["cliente_"] = $nombre;
 }
-//Si cliente está vacío se muestra el html de formulario de inicio 
-if (empty($_GET['cliente'])) {
 ?>
-    <!DOCTYPE html>
+ <!DOCTYPE html>
     <html lang="es">
 
     <head>
@@ -34,20 +26,21 @@ if (empty($_GET['cliente'])) {
 
     <body>
         <h1>La Frutería del siglo XXI</h1>
-        <br>
         <h2>BIENVENIDO A LA NUESTRA FRUTERÍA DEL SIGLO XXI</h2>
+<?php
+
+//Si cliente está vacío se muestra el html de formulario de inicio 
+if (!isset($_GET['cliente'])) {
+?>
         <form action="laFruteria.php" method="GET">
             <p>
                 <label for="">Introduzca su nombre de cliente: </label>
                 <input type="text" name="cliente">
             </p>
         </form>
-    </body>
-
-    </html>
 <?php
 //Si no está vacío se cogen los datos de cliente y el html de elegir frutas
-} elseif (!empty($_GET['cliente'])) {
+}else{
     
     //Si se pulsa anotar se crea sesión de frutas y se va acumulando el array
     if (isset($_POST["anotar"])) {
@@ -80,27 +73,13 @@ if (empty($_GET['cliente'])) {
             $_SESSION["listadecompra"][$elegida] = $cantidad;
         }
     }
-   
-    //Si se pulsa terminar se cierra la sesión
+//Si se pulsa terminar se cierra la sesión
     
 //Si cliente no está vacío se muestra el html con el nombre de cliene y con los datos de la lista de compra
 //Si existen datos de fruta se muestra el formulario con la lista del carrito
 
     if(isset($_POST['terminar'])){
     ?>
-     <!DOCTYPE html>
-    <html lang="es">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>frutería</title>
-    </head>
-
-    <body>
-        <h1>La Frutería del siglo XXI</h1>
-        <br>
         <h6>Este es su pedido:</h6>
         <table border="1">
             <th>Fruta</th>
@@ -123,19 +102,6 @@ if (empty($_GET['cliente'])) {
     }
     elseif(isset($_POST["fruta"])){
         ?>
-    <!DOCTYPE html>
-    <html lang="es">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>frutería</title>
-    </head>
-
-    <body>
-        <h1>La Frutería del siglo XXI</h1>
-        <br>
         <h6>Este es su pedido:</h6>
         <table border="1">
             <th>Fruta</th>
@@ -150,7 +116,7 @@ if (empty($_GET['cliente'])) {
             }
             ?>
         </table>
-        <h2>REALICE SU COMPRA <?php echo $_SESSION["cliente_"] ?></h2>
+        <h2>REALICE SU COMPRA <?php echo $nombre ?></h2>
         <form action="#" method="POST">
             <p>
                 <label for="">Selecciona la fruta: </label>
@@ -167,27 +133,12 @@ if (empty($_GET['cliente'])) {
             <input type="submit" name="anotar" value="ANOTAR">
             <input type="submit" name="terminar" value="TERMINAR">
         </form>
-    </body>
         <?php
 
     }elseif(!isset($_POST["fruta"])){
         //Si no existen datos de fruta se muestra el formulario sin datos del carrito
         ?>
-    <!DOCTYPE html>
-    <html lang="es">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>frutería</title>
-    </head>
-
-    <body>
-        <h1>La Frutería del siglo XXI</h1>
-        <br>
-    
-        <h2>REALICE SU COMPRA <?php echo $_SESSION["cliente_"] ?></h2>
+        <h2>REALICE SU COMPRA <?php echo $nombre ?></h2>
         <form action="#" method="POST">
             <p>
                 <label for="">Selecciona la fruta: </label>
@@ -197,18 +148,15 @@ if (empty($_GET['cliente'])) {
                     <option>Limones</option>
                 </select>
             </p>
-            
             <p>
                 <input type="number" name="cantidad">
             </p>
             <input type="submit" name="anotar" value="ANOTAR">
             <input type="submit" name="terminar" value="TERMINAR">
         </form>
-    </body>
-
-    </html>
 <?php
-        
     }
 }
 ?>
+   </body>
+ </html>
