562a69fd621553c0c209bc034e09311acb2a5eb7
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2021-11-28 19:53:38 +0100

    Update idiomas.php

--- SESSION_COOKIES/EJEMPLOS_COOKIES/idiomas/idiomas.php
+++ SESSION_COOKIES/EJEMPLOS_COOKIES/idiomas/idiomas.php
@@ -17,6 +17,10 @@ if(!isset($_GET["idioma"])){
     <br>
     <br>
     <a href="creaCookie.php?idioma=uk"><img src="uk.jpg" width="60" height="60"></a>
+    <form action="#" method="GET">
+        <input type="submit" name="terminar" value="TERMINAR">
+    </form>
+    
     </center>
 </body>
 </html>
@@ -24,6 +28,33 @@ if(!isset($_GET["idioma"])){
 
 
 <?php
+}
+
+if(!empty($_GET["terminar"])){
+    
+    ?>
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inglaterra</title>
+    </head>
+
+    <body>
+        <center>
+            <h1>GRACIAS POR TU VISITA TURÍSTICA</h1>
+            <img src="VIAJERO.jpg" width="200" height="200">
+            <br>
+        </center>
+    </body>
+
+    </html>
+    <?php
+
+
 }
 
 ?>
