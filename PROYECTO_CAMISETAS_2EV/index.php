23c4cb2790115d6480fcdce49f7d34c271030ee3
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-21 14:58:59 +0100

    Add files via upload

---
+++ PROYECTO_CAMISETAS_2EV/index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+//He quitado el autoload porque no me dejaba instalar el dompdf
+//He metido manial los controllers en el index
+require_once 'controllers/CarritoController.php';
+require_once 'controllers/CategoriaController.php';
+require_once 'controllers/ErrorController.php';
+require_once 'controllers/PedidoController.php';
+require_once 'controllers/ProductoController.php';
+require_once 'controllers/UsuarioController.php';
+//require_once 'autoload.php';
+require_once 'config/db.php';
+require_once 'config/parameters.php';
+require_once 'helpers/Utils.php';
+require_once 'views/layout/header.php';
+require_once 'views/layout/sidebar.php';
+
+
+function show_error(){
+	$error = new errorController();
+	$error->index();
+}
+
+if(isset($_GET['controller'])){
+	$nombre_controlador = $_GET['controller'].'Controller';
+
+}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+	$nombre_controlador = controller_default;
+	
+}else{
+	show_error();
+	exit();
+}
+
+if(class_exists($nombre_controlador)){	
+	$controlador = new $nombre_controlador();
+	
+	if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+		$action = $_GET['action'];
+		$controlador->$action();
+	}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+		$action_default = action_default;
+		$controlador->$action_default();
+	}else{
+		show_error();
+	}
+}else{
+	show_error();
+}
+
+require_once 'views/layout/footer.php';
