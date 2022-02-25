ab2020835fbc198a825e19acb5a12eb95e83fa4d
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-25 21:43:58 +0100

    Create ServletRegistro.java

---
+++ javaEE/Ejercicios_Servlet/formulario_pildoras/ServletRegistro.java
@@ -0,0 +1,48 @@
+package es.pildoras.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletRegistro
+ */
+@WebServlet("/ServletRegistro")
+public class ServletRegistro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletRegistro() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//Como el formulario es mediante get se genera la respuesta en get
+		PrintWriter salida=response.getWriter();
+		salida.println("<html><body>");
+		salida.println("Nombre introducido: "+request.getParameter("cNombre")+"<br>");
+		salida.println("Apellido introducido: "+request.getParameter("cApellido"));
+		salida.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
