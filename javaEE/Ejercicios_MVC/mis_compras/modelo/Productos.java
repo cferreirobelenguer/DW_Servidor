cb2bafd23666b1acb4c1332261d5629eeb7a8112
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-26 18:19:08 +0100

    Create Productos.java

---
+++ javaEE/Ejercicios_MVC/mis_compras/modelo/Productos.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Productos
+{
+	private String cod_pro;
+	private String nombre_pro;
+	private Integer precio;
+	
+	public String getCod_pro() {
+		return cod_pro;
+	}
+	public void setCod_pro(String cod_pro) {
+		this.cod_pro = cod_pro;
+	}
+	public String getNombre_pro() {
+		return nombre_pro;
+	}
+	public void setNombre_pro(String nombre_pro) {
+		this.nombre_pro = nombre_pro;
+	}
+	public Integer getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+	
+	
+}
