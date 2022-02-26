681b440ccf91a3035d1d7f419548d679b1c60097
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-26 19:58:05 +0100

    Create Usuario.java

---
+++ javaEE/Ejercicios_MVC/ejemploPildoras/modelo/Usuario.java
@@ -0,0 +1,55 @@
+package modelo;
+
+public class Usuario {
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String contra;
+	private String pais;
+	private String tecno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContra() {
+		return contra;
+	}
+	public void setContra(String contra) {
+		this.contra = contra;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getTecno() {
+		return tecno;
+	}
+	public void setTecno(String tecno) {
+		this.tecno = tecno;
+	}
+	
+	
+}
