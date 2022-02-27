49ce2341c36ae2b6b761cb7c42deb1462003e818
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-27 14:29:32 +0100

    Create Vehiculo.java

---
+++ javaEE/Ejercicios_MVC/mycar_EXAMEN/modelo/Vehiculo.java
@@ -0,0 +1,37 @@
+
+package modelo;
+
+public class Vehiculo
+{
+	private Integer cod_car;
+	private String localidad;
+	private Integer estado;
+	private Integer bateria;
+	
+	public Integer getCod_car() {
+		return cod_car;
+	}
+	public void setCod_car(Integer cod_car) {
+		this.cod_car = cod_car;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public Integer getEstado() {
+		return estado;
+	}
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+	public Integer getBateria() {
+		return bateria;
+	}
+	public void setBateria(Integer bateria) {
+		this.bateria = bateria;
+	}
+	
+	
+}
