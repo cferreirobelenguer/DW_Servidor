e4d1d86cf7ab87c9e35b89b6bbc36274deebe257
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-27 16:04:58 +0100

    Create clientes.java

---
+++ javaEE/Ejercicios_MVC/tienda_cookie/modelo/clientes.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public class clientes {
+	private String cod_cli;
+	private String nombre;
+	private String clave;
+	private int cod_cam;
+	public String getCod_cli() {
+		return cod_cli;
+	}
+	public void setCod_cli(String cod_cli) {
+		this.cod_cli = cod_cli;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public int getCod_cam() {
+		return cod_cam;
+	}
+	public void setCod_cam(int cod_cam) {
+		this.cod_cam = cod_cam;
+	}
+	
+	
+}
