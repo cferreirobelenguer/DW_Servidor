f25c2f0a69ba11f637c0fdf695bd71030971a331
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-27 16:05:18 +0100

    Create tiendas.java

---
+++ javaEE/Ejercicios_MVC/tienda_cookie/modelo/tiendas.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class tiendas {
+	private int cod_cam;
+	private String localidad;
+	private String productos;
+	public int getCod_cam() {
+		return cod_cam;
+	}
+	public void setCod_cam(int cod_cam) {
+		this.cod_cam = cod_cam;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getProductos() {
+		return productos;
+	}
+	public void setProductos(String productos) {
+		this.productos = productos;
+	}
+	
+	
+}
